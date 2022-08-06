3c570122668f999991de6b88f925cdbfa5b93159
Ibnu Tuharea <ibent95tuny@gmail.com>
2022-08-06 20:22:40 +0700

    Added material component to module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,15 @@
-import { NgModule } from '@angular/core';
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { registerLocaleData } from '@angular/common';
+import localeId from "@angular/common/locales/id";
 
 import { AppRoutingModule } from './app-routing.module';
+import { MaterialModule } from './material/material.module';
 import { AppComponent } from './app.component';
+import { AppService } from './services/app.service';
+
+registerLocaleData(localeId, 'id');
 
 @NgModule({
   declarations: [
@@ -10,9 +17,14 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'id-ID' },
+    AppService
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
